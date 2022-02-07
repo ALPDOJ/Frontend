19259af8687a21d4e0c9aba934962cf353a24f0b
true_mogician <925269713@qq.com>
2022-02-07 22:13:46 +0800

    feat: login page

--- Client/Models/LoginParamsType.cs
+++ Client/Models/LoginParamsType.cs
@@ -4,16 +4,10 @@ namespace Client.Models;
 
 public class LoginParamsType {
 	[Required]
-	public string UserName { get; set; }
+	public int Id { get; set; }
 
 	[Required]
 	public string Password { get; set; }
-
-	public string Mobile { get; set; }
-
-	public string Captcha { get; set; }
-
-	public string LoginType { get; set; }
 
 	public bool AutoLogin { get; set; }
 }

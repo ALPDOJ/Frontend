61f79344801d0f464861642488f37543ece76a1b
true_mogician <925269713@qq.com>
2022-02-16 23:07:01 +0800

    feat: DateTime friendly display

--- Client/Extensions/DateTimeExtension.cs
+++ Client/Extensions/DateTimeExtension.cs
@@ -1,4 +1,4 @@
-﻿namespace Client; 
+﻿namespace Client.Extensions;
 
 public static class DateTimeExtension {
 	private const int Second = 1;
@@ -9,33 +9,22 @@ public static class DateTimeExtension {
 
 	private const int Day = 24 * Hour;
 
-	private const int Month = 30 * Day;
+	private const double Month = 30.4375 * Day;
 
-	// todo: Need to be localized
+	private const double Year = 12 * Month;
+
 	public static string ToFriendlyDisplay(this DateTime dateTime) {
 		var ts = DateTime.Now - dateTime;
 		double delta = ts.TotalSeconds;
-		if (delta < 0)
-			return "not yet";
-		if (delta < 1 * Minute)
-			return ts.Seconds == 1 ? "1 second ago" : ts.Seconds + " seconds ago";
-		if (delta < 2 * Minute)
-			return "1 minute ago";
-		if (delta < 45 * Minute)
-			return ts.Minutes + "minute";
-		if (delta < 90 * Minute)
-			return "1 hour ago";
-		if (delta < 24 * Hour)
-			return ts.Hours + " hours ago";
-		if (delta < 48 * Hour)
-			return "yesterday";
-		if (delta < 30 * Day)
-			return ts.Days + " days ago";
-		if (delta < 12 * Month) {
-			var months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
-			return months <= 1 ? "A month ago" : months + " months ago";
-		}
-		var years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
-		return years <= 1 ? "a year ago" : years + " years ago";
+		string suffix = delta < 0 ? "后" : "前";
+		return Math.Abs(delta) switch {
+				< Minute => $"{ts.Seconds}秒",
+				< Hour   => $"{ts.Minutes}分钟",
+				< Day    => $"{ts.Hours}小时",
+				< Month  => $"{ts.Days}天",
+				< Year   => $"{(int)Math.Floor(Math.Abs(delta) / Month)}月",
+				_        => $"{(int)Math.Floor(Math.Abs(delta) / Year)}年"
+			} +
+			suffix;
 	}
 }

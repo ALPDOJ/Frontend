186e19cdaac43e162f58c450d9018586007ddd1a
true_mogician <925269713@qq.com>
2022-02-21 16:21:12 +0800

    feat: UploadModal

---
+++ Client/Pages/Problem/Components/UploadModalArgument.cs
@@ -0,0 +1,11 @@
+﻿namespace Client.Pages.Problem;
+
+public class UploadModalArgument {
+	public bool Multiple { get; set; }
+
+	public string Accept { get; set; }
+
+	public long? MaxFileSize { get; set; }
+
+	public long? TotalFileSize { get; set; }
+}
---
+++ Client/Utils/Formatter.cs
@@ -0,0 +1,13 @@
+﻿namespace Client.Utils;
+
+public static class Formatter {
+	public static string FormatSize(long size, int precision = 1) {
+		string format = precision == 0 ? "##" : "##." + new string('#', precision);
+		return size switch {
+			< 1 << 10 => $"{size} B",
+			< 1 << 20 => $"{((double)size / (1 << 10)).ToString(format)} KB",
+			< 1 << 30 => $"{((double)size / (1 << 20)).ToString(format)} MB",
+			_         => $"{((double)size / (1 << 30)).ToString(format)} GB"
+		};
+	}
+}

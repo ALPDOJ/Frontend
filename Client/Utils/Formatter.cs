15af88f2232b6fa860e3f33e9e9e7ba855fe46e3
true_mogician <925269713@qq.com>
2022-02-21 22:49:43 +0800

    refactor: move to Formatter

--- Client/Utils/Formatter.cs
+++ Client/Utils/Formatter.cs
@@ -1,4 +1,6 @@
-﻿namespace Client.Utils;
+﻿using System.Text;
+
+namespace Client.Utils;
 
 public static class Formatter {
 	public static string FormatSize(long size, int precision = 1) {
@@ -9,5 +11,19 @@ public static class Formatter {
 			< 1 << 30 => $"{((double)size / (1 << 20)).ToString(format)} MB",
 			_         => $"{((double)size / (1 << 30)).ToString(format)} GB"
 		};
+	}
+
+	public static string FormatDateTimeRelatively(DateTime dateTime, int precision = 1) {
+		var span = dateTime - DateTime.Now;
+		string suffix = span < TimeSpan.Zero ? "前" : "后";
+		span = span.Duration();
+		var components = new List<string>();
+		components.Add($"{span.Days}天");
+		components.Add($"{span.Hours}小时");
+		components.Add($"{span.Minutes}分钟");
+		components.Add($"{span.Seconds}秒");
+		components.Add($"{span.Milliseconds}毫秒");
+		int idx = components.FindIndex(c => !c.StartsWith("0"));
+		return string.Join(null, components.Skip(idx).Take(precision + 1)) + suffix;
 	}
 }

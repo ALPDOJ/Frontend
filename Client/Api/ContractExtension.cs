51632d5c1b065469a1fdc3bb3482895b0637863a
true_mogician <925269713@qq.com>
2022-02-28 16:05:56 +0800

    feat: User.GetDisplayingName

--- Client/Api/ContractExtension.cs
+++ Client/Api/ContractExtension.cs
@@ -2,4 +2,6 @@
 
 public static class ContractExtension {
 	public static float GetTotalScore(this ProblemSubmissionLimit limit) => limit.TotalScore ?? limit.TestCaseScores!.Values.Sum();
+
+	public static string GetDisplayingName(this User user) => user.Username ?? user.Id.ToString();
 }

d47a478044cca0cedc6cfe8a7d9ac48d7edf5804
true_mogician <925269713@qq.com>
2022-03-03 22:41:34 +0800

    feat: ErrorHandler

---
+++ Client/Api/ErrorHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Client.Api;
+
+public class ErrorHandler {
+	private ApiClient Api { get; }
+
+	private NavigationManager Navigator { get; }
+
+	public ErrorHandler(ApiClient api, NavigationManager navigator) {
+		Api = api;
+		Navigator = navigator;
+	}
+
+	public void Handle(ApiException exception, params int[] statusCodes) {
+		if (statusCodes.Length > 0 && !statusCodes.Contains(exception.StatusCode))
+			return;
+		switch (exception.StatusCode) {
+			case 401:
+				HandleUnauthorized(exception);
+				break;
+			case 403:
+				HandleForbidden(exception);
+				break;
+		}
+	}
+
+	private void HandleUnauthorized(ApiException exception) {
+		if (exception.StatusCode == 401)
+			Navigator.NavigateTo("/login");
+	}
+
+	private void HandleForbidden(ApiException exception) {
+		if (exception.StatusCode != 403)
+			return;
+		Navigator.NavigateTo("/exception?status=403");
+	}
+}

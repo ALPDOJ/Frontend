65efaac4082fc74c4dac300a24509c34e56ac883
true_mogician <925269713@qq.com>
2022-03-07 03:49:15 +0800

    feat: complete DiscussionView

--- Client/Api/ContractExtension.cs
+++ Client/Api/ContractExtension.cs
@@ -4,4 +4,9 @@ public static class ContractExtension {
 	public static float GetTotalScore(this ProblemSubmissionLimit limit) => limit.TotalScore ?? limit.TestCaseScores!.Values.Sum();
 
 	public static string GetDisplayingName(this User user) => user.Username ?? user.Id.ToString();
+
+	public static string GetDisplayingName(this Author author) => author.Username ?? author.Id.ToString();
+
+	public static int GetReactionCount(this Comment comment, Reaction reaction)
+		=> Enum.GetName(reaction) is { } key && comment.Reactions?.ContainsKey(key) == true ? comment.Reactions[key].Count : 0;
 }

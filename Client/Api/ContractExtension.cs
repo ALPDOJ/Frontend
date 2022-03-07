9a44a3220b72a334558beec961a6842fd79b2d1e
true_mogician <925269713@qq.com>
2022-03-07 22:57:26 +0800

    perf: comment

--- Client/Api/ContractExtension.cs
+++ Client/Api/ContractExtension.cs
@@ -5,7 +5,7 @@ public static class ContractExtension {
 
 	public static string GetDisplayingName(this User user) => user.Username ?? user.Id.ToString();
 
-	public static string GetDisplayingName(this Author author) => author.Username ?? author.Id.ToString();
+	public static string GetDisplayingName(this CommentAuthor author) => author.Username ?? author.Id.ToString();
 
 	public static int GetReactionCount(this Comment comment, Reaction reaction)
 		=> Enum.GetName(reaction) is { } key && comment.Reactions?.ContainsKey(key) == true ? comment.Reactions[key].Count : 0;

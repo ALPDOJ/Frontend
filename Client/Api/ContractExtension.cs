32d2450e9fd560b49e61d16c12f856d0dd95a0e8
true_mogician <925269713@qq.com>
2022-03-08 17:15:28 +0800

    feat: SubmissionView

--- Client/Api/ContractExtension.cs
+++ Client/Api/ContractExtension.cs
@@ -9,4 +9,13 @@ public static class ContractExtension {
 
 	public static int GetReactionCount(this Comment comment, Reaction reaction)
 		=> Enum.GetName(reaction) is { } key && comment.Reactions?.ContainsKey(key) == true ? comment.Reactions[key].Count : 0;
+
+	public static double GetTotalScore(this ScoreSummary score) {
+		double result = 0;
+		if (score.Judgement is not null)
+			result = score.Judgement.Values.Average();
+		if (score.Review is not null)
+			result += score.Review.Value;
+		return result;
+	}
 }

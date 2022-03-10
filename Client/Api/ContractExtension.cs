0e6414a7d3da1c80f54a6f26b61293d3c8044e81
true_mogician <925269713@qq.com>
2022-03-11 03:45:51 +0800

    chore: naming

--- Client/Api/ContractExtension.cs
+++ Client/Api/ContractExtension.cs
@@ -1,7 +1,7 @@
 ﻿namespace Client.Api;
 
 public static class ContractExtension {
-	public static float GetTotalScore(this ProblemSubmissionLimit limit) => limit.TotalScore ?? limit.TestCaseScores!.Values.Sum();
+	public static float GetTotalScore(this ProblemSubmissionLimit limit) => limit.TotalScore ?? limit.TestcaseScores!.Values.Sum();
 
 	public static string GetDisplayingName(this User user) => user.Username ?? user.Id.ToString();
 

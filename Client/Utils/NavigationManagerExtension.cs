4548fbaf359e02e3a354cdcbb27185d585a5a302
true_mogician <925269713@qq.com>
2022-03-13 01:11:36 +0800

    feat: ChangePasswordResultPage

---
+++ Client/Utils/NavigationManagerExtension.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Client.Utils;
+
+public static class NavigationManagerExtension {
+	public static void NavigateTo(this NavigationManager manager, Uri uri, NavigationOptions options) => manager.NavigateTo(uri.ToString(), options);
+
+	public static void NavigateTo(this NavigationManager manager, Uri uri) => manager.NavigateTo(uri.ToString());
+}
---
+++ Client/Utils/Utilities.cs
@@ -0,0 +1,6 @@
+﻿namespace Client.Utils;
+
+public static class Utilities {
+	public static string ToQueryString(this Dictionary<string, string> queries)
+		=> string.Join('&', queries.Select(pair => $"{Uri.EscapeDataString(pair.Key)}={Uri.EscapeDataString(pair.Value)}"));
+}

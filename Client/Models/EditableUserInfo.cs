9ebcab8285d9fd27fd620cdec11d872d310b2505
true_mogician <925269713@qq.com>
2022-03-15 02:53:32 +0800

    feat: AccountPage.BasicView

---
+++ Client/Models/EditableUserInfo.cs
@@ -0,0 +1,18 @@
+﻿using Client.Api;
+
+namespace Client.Models;
+
+public class EditableUserInfo {
+	public EditableUserInfo() { }
+
+	public EditableUserInfo(User user) {
+		Username = user.Username;
+		Email = user.Email;
+	}
+
+	public string? Username { get; set; }
+
+	public string? Password { get; set; }
+
+	public string? Email { get; set; }
+}

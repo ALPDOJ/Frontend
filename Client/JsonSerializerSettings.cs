9b827f12ef11279e731c546753756006d17362c4
FeO3 <seashell11234455@gmail.com>
2023-02-28 17:01:16 +0800

    fix: bug fix

---
+++ Client/JsonSerializerSettings.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Client.Api
+{
+    partial class ApiClient
+    {
+        partial void UpdateJsonSerializerSettings(JsonSerializerSettings settings)
+        {
+            settings.DateFormatString = "yyyy-MM-ddTHH:mm:ss";
+        }
+    }
+}
